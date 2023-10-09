384ec05101055248e45cbcceada07e6541ed0707
Gilbert Omosa <gilbert.omosa@student.moringaschool.com>
2023-10-09 09:03:17 +0300

    Done with Assignment

--- main.js
+++ main.js
@@ -4,8 +4,42 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+document.addEventListener("DOMContentLoaded", () => {
+  // Step 1: Add the .hidden class to the error modal
+  const errorModal = document.getElementById("modal");
+  errorModal.classList.add("hidden");
+  
+  // Step 2: Add click event listeners to empty hearts
+  document.querySelectorAll(".like-glyph").forEach((heart) => {
+    heart.addEventListener("click", () => {
+      mimicServerCall()
+        .then(() => {
+          // Step 3: Change the heart to a full heart
+          if (heart.classList.contains("activated-heart")) {
+            heart.classList.remove("activated-heart");
+            heart.innerText = EMPTY_HEART;
+          } else {
+            heart.classList.add("activated-heart");
+            heart.innerText = FULL_HEART;
+          }
+        })
+        .catch((error) => {
+          // Step 4: Handle the error
+          const errorMessage = document.getElementById("modal-message");
+          
+          // Step 5: Display the error modal and message
+          errorMessage.textContent = error;
+          errorModal.classList.remove("hidden");
+  
+          // Step 6: Use setTimeout to hide the modal after 3 seconds
+          setTimeout(() => {
+            errorModal.classList.add("hidden");
+          }, 3000);
+        });
+    });
+  });
 
-
+});
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
